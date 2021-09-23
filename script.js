90faf141807d2c8d29a751c0a0931df35902d4b9
バターネコ <71169815+HYM740@users.noreply.github.com>
2021-09-23 12:50:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ var imi = document.querySelector("#imi")
 var rei = document.querySelector("#rei")
 var jsonfile = document.querySelector("#json")
 var nextBotton = document.querySelector("#next")
+var tangoselectdiv = document.querySelector("#tangoselect")
+var ts = document.querySelector("#ts")
 var jsonstr;
 var tangocyou;
 jsonfile.addEventListener("change", reader);
 nextBotton.addEventListener("click",randomData);
+ts.addEventListener("change",()=>console.log(ts.selectedIndex))
 function reader() {
     var reader = new FileReader();
     reader.readAsText(this.files[0]);
     reader.onload = function fileReadCompleted() {
         jsonstr = reader.result;
         tangocyou = JSON.parse(jsonstr)
-        putOnData();
+        randomData();
+        tangoselectdiv.style.display="none"
     }
-}
-function putOnData(){
-    console.log("load");
-    tango.innerHTML=tangocyou[0].omote;
-    yumigata.innerHTML=tangocyou[0].uchi;
-    imi.innerHTML=tangocyou[0].imi;
-    rei.innerHTML=tangocyou[0].rei;
 }
 function randomData(){
     var index=Math.round(Math.random()*tangocyou.length);
